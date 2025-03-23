e3de8ea0bfced12895f337da770119897ef855fd
Manasseh <judithnnadi98@gmail.com>
2025-03-23 20:22:22 +0100

    update while loop

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 import cv2
-import mediapipe as mp
 import pyautogui
-cam = cv2.VideoCapture(0)
+import mediapipe as mp
+
+
+
+camr = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size()
-while True:
-    _, frame = cam.read()
+var = 1
+while var:
+    _, frame = camr.read()
     frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mesh.process(rgb_frame)
